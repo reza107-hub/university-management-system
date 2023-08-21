5041db25ee7c17d6a9c7b5f630c8f77f7403608b
Antor-094 <mdantor556677889@gmail.com>
2023-08-21 19:40:31 +0600

    Banner contant animation added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,4 +1,3 @@
-// import React from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import {
   Navigation,
@@ -13,16 +12,44 @@ import "swiper/css/pagination";
 import "swiper/css/mousewheel";
 import "swiper/css/keyboard";
 import "./BannerStyle.css";
-import { useEffect } from "react";
+import { useState } from "react";
 
 const Banner = () => {
-  useEffect(() => {
-    // Add the 'active' class to the content container of the first slide when the component mounts
-    const firstSlideContent = document.querySelector('.slide-content');
-    if (firstSlideContent) {
-      firstSlideContent.classList.add('active');
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+
+  const content = [
+    {
+      title: "Welcome to Sylhet Metropolitan University!",
+      sub_title: "Empowering Minds, Building Futures",
+      image: "https://i.ibb.co/T8zf4T2/EKbrbk3-UEAAHVF1.jpg",
+    },
+    {
+      title: "Join a Community of Learning",
+      sub_title: "Transforming Dreams into Reality",
+      image: "https://i.ibb.co/dgQYzGS/Whats-App-Image-2023-08-18-at-7-35-41-PM.jpg",
+    },
+    {
+      title: "Explore a World of Opportunities",
+      sub_title: "Choose Sylhet Metropolitan University",
+      image: "https://i.ibb.co/MDyN5XL/323113454-1325448301586663-5004010928649462078-n.jpg",
+    },
+    {
+      title: "Discover Your Opportunities",
+      sub_title: "Admission Season is Here",
+      image: "https://i.ibb.co/BtTGZKF/metropolitan-university.jpg",
+    },
+  ];
+
+  const handleSlideChange = (swiper) => {
+    setActiveSlideIndex(swiper.activeIndex);
+
+    // Check if it's the last slide
+    if (swiper.isEnd) {
+      // Reset the active slide index to the first slide
+      setActiveSlideIndex(0);
     }
-  }, []);
+  };
+
   return (
     <div className="h-screen">
       <Swiper
@@ -30,112 +57,61 @@ const Banner = () => {
         navigation={true}
         pagination={true}
         mousewheel={true}
+        mousewheelSensitivity={0.5}
         keyboard={true}
         autoplay={{
           delay: 5000,
           disableOnInteraction: false,
         }}
+        onSlideChange={handleSlideChange}
         modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
         className="mySwiper"
-        onSlideChange={(swiper) => {
-          // Remove the 'active' class from all content containers
-          document.querySelectorAll(".slide-content").forEach((content) => {
-            content.classList.remove("active");
-          });
-
-          // Add the 'active' class to the content container of the current slide
-          const currentSlide = swiper.slides[swiper.activeIndex];
-          currentSlide.querySelector(".slide-content").classList.add("active");
-        }}
+        speed={2000}
+        hardwareAcceleration={true}
+        ease="ease-in-out"
       >
-        <SwiperSlide>
-          <div className="relative h-screen">
-            <img
-              src="https://i.ibb.co/T8zf4T2/EKbrbk3-UEAAHVF1.jpg"
-              alt=""
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center  text-white">
-              <div className="slide-content transition-opacity text-center ">
-                <h2 className="md:text-5xl text-2xl font-black font-raleway">
-                  Welcome to Sylhet Metropolitan University!
-                </h2>
-                <p className="md:text-lg mt-4 font-sans">
-                  Empowering Minds, Building Futures
-                </p>
-                <button className="m-[10px] px-[20px] py-[10px] bg-primary text-bold rounded-sm">
-                  Get Admission
-                </button>
+        {content.map((c, i) => (
+          <SwiperSlide key={i}>
+            <div className="relative h-screen">
+              <img
+                src={c?.image}
+                alt=""
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center  text-white">
+                <div className="text-center">
+                  <h2
+                    className={`md:text-5xl text-2xl font-black font-raleway ${
+                      activeSlideIndex === i
+                        ? "slide-animation slide-animation-left"
+                        : ""
+                    }`}
+                  >
+                    {c?.title}
+                  </h2>
+                  <p
+                    className={`md:text-lg mt-4 font-sans ${
+                      activeSlideIndex === i
+                        ? "slide-animation slide-animation-right"
+                        : ""
+                    }`}
+                  >
+                    {c?.sub_title}
+                  </p>
+                  <button
+                    className={`m-[10px] px-[20px] py-[10px] bg-primary text-bold rounded-sm ${
+                      activeSlideIndex === i
+                        ? "slide-animation slide-animation-down"
+                        : ""
+                    }`}
+                  >
+                    Get Admission
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative h-screen">
-            <img
-              src="https://i.ibb.co/MDyN5XL/323113454-1325448301586663-5004010928649462078-n.jpg"
-              alt=""
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-white">
-              <div className="slide-content transition-opacity text-center">
-                <h2 className="md:text-5xl text-2xl font-black font-raleway">
-                  Join a Community of Learning
-                </h2>
-                <p className="md:text-lg mt-4 font-sans">
-                  Transforming Dreams into Reality
-                </p>
-                <button className="m-[10px] px-[20px] py-[10px] bg-primary text-bold rounded-sm">
-                  Get Admission
-                </button>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative h-screen">
-            <img
-              src="https://i.ibb.co/dgQYzGS/Whats-App-Image-2023-08-18-at-7-35-41-PM.jpg"
-              alt=""
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-white">
-              <div className="slide-content transition-opacity text-center">
-                <h2 className="md:text-5xl text-2xl font-black font-raleway">
-                  Explore a World of Opportunities
-                </h2>
-                <p className="md:text-lg mt-4 font-sans">
-                  Choose Sylhet Metropolitan University
-                </p>
-                <button className="m-[10px] px-[20px] py-[10px] bg-primary text-bold rounded-sm">
-                  Get Admission
-                </button>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative h-screen">
-            <img
-              src="https://i.ibb.co/BtTGZKF/metropolitan-university.jpg"
-              alt=""
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-white">
-              <div className="slide-content transition-opacity text-center">
-                <h2 className="md:text-5xl text-2xl font-black font-raleway">
-                  Discover Your Opportunities
-                </h2>
-                <p className="md:text-lg mt-4 font-sans">
-                  Admission Season is Here
-                </p>
-                <button className="m-[10px] px-[20px] py-[10px] bg-primary text-bold rounded-sm">
-                  Get Admission
-                </button>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
