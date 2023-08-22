2f9dfc1cb71749b2f03c98604ebf37b74570cdf8
reza107-hub <rashidaminur104@gmail.com>
2023-08-22 19:44:19 +0600

    Navbar responsive done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,15 @@
 import Banner from "./Banner/Banner";
+import Test from "./Test/Test";
 import UniversityStatistics from "./UniversityStatistics/UniversityStatistics";
 
-
 const Home = () => {
-    return (
-        <>
-            <Banner />
-            <UniversityStatistics />
-        </>
-    );
+  return (
+    <>
+      <Banner />
+      <UniversityStatistics />
+      <Test />
+    </>
+  );
 };
 
 export default Home;
-
--- src/Pages/Shared/Navbar/SmallScreenView.jsx
+++ src/Pages/Shared/Navbar/SmallScreenView.jsx
@@ -1,17 +1,55 @@
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
 
 const SmallScreenView = ({ navigation, Disclosure }) => {
   return (
     <div className="space-y-1 px-2 pb-3 pt-2">
-      {navigation.map((item) => (
-        <Disclosure.Button
-          key={item.name}
-          as={Link}
-          to={item.to || "#"} // Use item.to if available, or "#" as a placeholder
-          className="text-primary hover:bg-primary hover:text-white block rounded-md px-3 py-2 text-base font-bold"
-        >
-          {item.name}
-        </Disclosure.Button>
+      {navigation.map((item, index) => (
+        // accordion starting
+        <Disclosure key={index} as={Link} to={item?.to}>
+          {({ open }) => (
+            <>
+              {/* accordion click button */}
+              <Disclosure.Button className="text-white hover:bg-white hover:text-primary rounded-md px-3 py-2 text-base font-bold flex justify-between w-full">
+                <span>{item?.name}</span>
+                {item?.nested_links ? (
+                  <>
+                    {/* accordion arrow sign */}
+                    <ChevronDownIcon
+                      className={`${
+                        open ? "rotate-180 transform" : ""
+                      } h-5 w-5 text-purple-500`}
+                    />
+                  </>
+                ) : (
+                  <></>
+                )}
+              </Disclosure.Button>
+              {/* accordion content */}
+              {item?.nested_links ? (
+                <>
+                  <Disclosure.Panel className="text-white hover:bg-white hover:text-primary rounded-md px-3 py-2 text-base font-bold">
+                    {item?.nested_links ? (
+                      <>
+                        {item?.nested_links.map((link) => (
+                          <Link key={link.name} to={link.to}>
+                            <li className="hover:bg-primary hover:text-white rounded-md px-3 py-2 text-base font-bold cursor-pointer list-none">
+                              {link.name}
+                            </li>
+                          </Link>
+                        ))}
+                      </>
+                    ) : (
+                      <></>
+                    )}
+                  </Disclosure.Panel>
+                </>
+              ) : (
+                <></>
+              )}
+            </>
+          )}
+        </Disclosure>
       ))}
     </div>
   );
