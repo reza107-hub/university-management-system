d8da14b56d975ca040708400943db50aa4adf37e
reza107-hub <rashidaminur104@gmail.com>
2023-11-12 20:03:57 +0600

    sign up page innitilized

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,21 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main/Main";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
+import Register from "../Pages/Regester/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    children:[
+    children: [
       {
-        path:'/',
-        element:<Home />
+        path: "/",
+        element: <Home />,
       },
-      {
-        path:'/login',
-        element:<Login />
-      },
-    ]
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 
