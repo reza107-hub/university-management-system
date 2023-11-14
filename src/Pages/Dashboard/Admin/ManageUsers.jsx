f2cd6e4bd923ba9ef3348802b144f3129c77087e
reza107-hub <rashidaminur104@gmail.com>
2023-11-14 07:57:56 +0600

    admin can make a user admin

---
+++ src/Pages/Dashboard/Admin/ManageUsers.jsx
@@ -0,0 +1,123 @@
+import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
+
+const ManageUsers = () => {
+  const { data: users = [], refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/users");
+      return res.json();
+    },
+  });
+
+  const handleMakeAdmin = (user) => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an Admin Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
+  return (
+    <div>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
+          <label className="sr-only">Search</label>
+          <div className="relative">
+            <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="table-search-users"
+              className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search for users"
+            />
+          </div>
+        </div>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                email
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Role
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map((user) => (
+              <tr
+                key={user?._id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <th
+                  scope="row"
+                  className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  <img
+                    className="w-10 h-10 rounded-full"
+                    src={user?.image}
+                    alt={`Profile of ${user?.name}`}
+                  />
+                  <div className="ps-3">
+                    <div className="text-base font-semibold">{user?.name}</div>
+                  </div>
+                </th>
+                <td className="px-6 py-4">{user?.email}</td>
+                <td className="px-6 py-4">{user?.role}</td>
+                <td className="px-6 py-4">
+                  {user?.role === "admin" ? (
+                    "admin"
+                  ) : (
+                    <button
+                      disabled={user?.role}
+                      onClick={() => handleMakeAdmin(user)}
+                      className={`btn-primary`}
+                    >
+                      Make Admin
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ManageUsers;
--- src/Pages/Shared/Navbar/Profile.jsx
+++ src/Pages/Shared/Navbar/Profile.jsx
@@ -1,4 +1,13 @@
-const Profile = ({ Menu, Transition, Fragment }) => {
+import { Link } from "react-router-dom";
+import useAuth from "../../../Hooks/useAuth";
+
+const Profile = ({ Menu, Transition, Fragment, user }) => {
+  const { logOut } = useAuth();
+
+  const handleLogOut = () => {
+    logOut();
+  };
+
   return (
     <>
       <Menu as="div" className="relative ml-3">
@@ -6,11 +15,7 @@ const Profile = ({ Menu, Transition, Fragment }) => {
           <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
             <span className="absolute -inset-1.5" />
             <span className="sr-only">Open user menu</span>
-            <img
-              className="h-8 w-8 rounded-full"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
+            <img className="h-8 w-8 rounded-full" src={user?.photoURL} alt="" />
           </Menu.Button>
         </div>
         <Transition
@@ -23,31 +28,21 @@ const Profile = ({ Menu, Transition, Fragment }) => {
           leaveTo="transform opacity-0 scale-95"
         >
           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-primary text-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={`${
-                    active ? "bg-gray-100 text-primary" : ""
-                  } block px-4 py-2 text-sm`}
-                >
-                  Your Profile
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={`${
-                    active ? "bg-gray-100 text-primary" : ""
-                  } block px-4 py-2 text-sm`}
-                >
-                  Settings
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
+            <Link to={"dashboard"}>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={`${
+                      active ? "bg-gray-100 text-primary" : ""
+                    } block px-4 py-2 text-sm`}
+                  >
+                    Dashboard
+                  </a>
+                )}
+              </Menu.Item>
+            </Link>
+            <Menu.Item onClick={handleLogOut}>
               {({ active }) => (
                 <a
                   href="#"
